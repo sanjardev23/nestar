d421d693ebee58c7b71f5f0d5ddddd4b242b04b1
sanjardev23 <mksanjar04@gmail.com>
2026-09-06 15:50:24 +0900

    fix: modify like interaction related logic

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -5,7 +5,7 @@ import { Model, ObjectId, Types } from 'mongoose';
 import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 import { T } from '../../libs/types/common';
 
 @Injectable()
@@ -74,7 +74,7 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId, '$followingId'), // meLiked
 							// meFollowed
 							lookupFollowingData,
 							{ $unwind: '$followingData' },
@@ -106,7 +106,7 @@ export class FollowService {
 						list: [
 							{ $skip: (page - 1) * limit },
 							{ $limit: limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId, '$followerId'), // meLiked
 							// meFollowed
 							lookupFollowerData,
 							{ $unwind: '$followerData' },
