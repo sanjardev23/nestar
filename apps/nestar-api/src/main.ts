3465c558cc340bed77090b41d40aca00b7e97769
sanjardev23 <mksanjar04@gmail.com>
2026-09-09 18:02:47 +0900

    fix: modify job scheduler business logic

--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
 	app.enableCors({ origin: true, credentials: true });
 
 	app.use(graphqlUploadExpress({ maxFileSize: 15000000, maxFiles: 10 }));
-	app.use('uploads', express.static('uploads'));
+	app.use('/uploads', express.static('uploads'));
 
 	app.useWebSocketAdapter(new WsAdapter(app));
 	await app.listen(process.env.PORT_API ?? 3000);
--- apps/nestar-batch/src/batch.module.ts
+++ apps/nestar-batch/src/batch.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
-import { BatchController } from './nestar-batch.controller';
 import { BatchService } from './batch.service';
 import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
 import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BatchController } from './nestar-batch.controller';
+import MemberSchema from '../../nestar-api/src/schemas/Member.model';
+import PropertySchema from '../../nestar-api/src/schemas/Property.model';
 
 @Module({
-	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		DatabaseModule,
+		ScheduleModule.forRoot(),
+		MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }]),
+		MongooseModule.forFeature([{ name: 'Member', schema: MemberSchema }]),
+	],
 	controllers: [BatchController],
 	providers: [BatchService],
 })
